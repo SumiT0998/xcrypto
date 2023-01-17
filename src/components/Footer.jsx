72a7c2d165ab79da3f923dd320531893c7032c23
unknown <sumit.bisht36@gmail.com>
2023-01-17 23:46:54 +0530

    final

---
+++ src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+
+const avatarSrc = "https://media.licdn.com/dms/image/C4D03AQF5nMvp55hKHQ/profile-displayphoto-shrink_800_800/0/1656182328244?e=1679529600&v=beta&t=2CVNGwSxV-kc4hviTjxVNoJK9b0tI7a-45yalYnYgF4";
+
+const Footer = () => {
+  return (
+    <Box
+      bgColor={"blackAlpha.900"}
+      color={"whiteAlpha.700"}
+      minH={"48"}
+      px={"16"}
+      py={["16", "8"]}
+    >
+      <Stack direction={["column", "row"]} h={"full"} alignItems={"center"}>
+        <VStack w={"full"} alignItems={["center", "flex-start"]}>
+          <Text fontWeight={"bold"}>About Us</Text>
+          <Text
+            fontSize={"sm"}
+            letterSpacing={"widest"}
+            textAlign={["center", "left"]}
+          >
+            We are the best crypto trading app in India, we provide our guidance
+            at a very cheap price.
+          </Text>
+        </VStack>
+
+        <VStack>
+          <Avatar boxSize={"28"} mt={["4", "0"]} src={avatarSrc} />
+          <Text>Sumit.s.Bisht</Text>
+        </VStack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
